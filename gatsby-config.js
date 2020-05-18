1d1a42454402d561bbde83f2a907e8a711563548
Jaewook Ahn <ajw4586@gmail.com>
2020-05-18 22:18:50 +0900

    Fix: production build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: `Jaewook's point of view`,
-    name: 'Jaewook Ahn',
-    author: 'Jaewook Ahn',
-    description: `Post all about Jaewook's point of view. Write code, memorable moment, take photo, share lifestyle, experience.`,
-    siteUrl: 'https://jaewoook.github.io',
-    hero: {
-      heading: 'Point of view',
-      maxWidth: 720,
+    siteMetadata: {
+        title: `Jaewook's point of view`,
+        name: 'Jaewook Ahn',
+        description: `Post all about Jaewook's point of view. Write code, memorable moment, take photo, share lifestyle, experience.`,
+        siteUrl: 'https://jaewoook.github.io',
+        hero: {
+            heading: 'Point of view',
+            maxWidth: 768,
+        },
+        social: [
+            {
+                name: 'github',
+                url: 'https://github.com/Jaewoook',
+            },
+        ],
     },
-    social: [
-      {
-        name: 'github',
-        url: 'https://github.com/Jaewoook',
-      },
+    pathPrefix: 'jaewoook',
+    plugins: [
+        {
+            resolve: '@narative/gatsby-theme-novela',
+            options: {
+                basePath: "/",
+                authorsPage: true,
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Jaewook\'s point of view',
+                short_name: 'Jaewook Ahn\'s blog',
+                short_url: '/',
+                background_color: `#fff`,
+                theme_color: `#fff`,
+                display: `standalone`,
+            },
+        },
     ],
-  },
-  pathPrefix: 'jaewoook',
-  plugins: [
-    '@narative/gatsby-theme-novela'
-  ],
 }
