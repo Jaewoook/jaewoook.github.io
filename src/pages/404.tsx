64a495e9b2602063937d295a18606c1521524c22
Jaewook Ahn <dev.jaewook@gmail.com>
2022-08-17 04:34:58 +0900

    update: 404 not found page
    - apply siteUrl graphql query
    - update design (WIP)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,35 @@
+/**
+* External modules
+*/
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
-import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
-import Section from "@narative/gatsby-theme-novela/src/components/Section";
-import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
-import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
+/**
+* Internal modules
+*/
+import Layout from "../components/Layout";
 
-function NotFoundPage() {
-  return (
-    <Layout>
-      <SEO />
-      <Section>
-        <div style={{ marginTop: "100px", display: "flex", justifyContent: "center" }}>
-          <Headings.h1>404 Not Found</Headings.h1>
-        </div>
-      </Section>
-    </Layout>
-  );
+const query = graphql`
+query {
+    site {
+        siteMetadata {
+            siteUrl
+        }
+    }
 }
+`;
+
+const NotFoundPage = () => {
+    const queryResult = useStaticQuery(query);
+
+    return (
+        <Layout>
+            <div className="mt-32 flex flex-col items-center justify-center">
+                <h1 className="text-2xl">페이지를 찾을 수 없어요</h1>
+                <button className="mt-5" onClick={() => window.location.href = queryResult.site.siteMetadata.siteUrl}>메인으로 가기</button>
+            </div>
+        </Layout>
+    );
+};
 
 export default NotFoundPage;
