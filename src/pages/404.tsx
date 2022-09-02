6ffaffb02c7f87b8f0e7cfe78721a2e8d6f1c354
Jaewook Ahn <dev.jaewook@gmail.com>
2022-09-03 01:48:21 +0900

    update: remove Layout from 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ import { graphql, useStaticQuery } from "gatsby";
 /**
 * Internal modules
 */
-import Layout from "../components/Layout";
+import { Button } from "../components/Button";
 
 const query = graphql`
 query {
@@ -23,12 +23,10 @@ const NotFoundPage = () => {
     const queryResult = useStaticQuery(query);
 
     return (
-        <Layout>
-            <div className="mt-32 flex flex-col items-center justify-center">
-                <h1 className="text-2xl">페이지를 찾을 수 없어요</h1>
-                <button className="mt-5" onClick={() => window.location.href = queryResult.site.siteMetadata.siteUrl}>메인으로 가기</button>
-            </div>
-        </Layout>
+        <div className="mt-32 mb-10 flex flex-col items-center justify-center">
+            <h1 className="text-2xl">페이지를 찾을 수 없어요.</h1>
+            <Button className="mt-5" onClick={() => window.location.href = queryResult.site.siteMetadata.siteUrl}>메인으로 가기</Button>
+        </div>
     );
 };
 
