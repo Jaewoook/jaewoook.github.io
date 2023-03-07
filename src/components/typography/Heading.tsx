107b65dbd8251ca517f00bff8befefe95742363c
Jaewook Ahn <dev.jaewook@gmail.com>
2023-03-08 04:48:19 +0900

    feat(typo): Add typography components
    - heading
    - code
    - to be more

---
+++ src/components/typography/Heading.tsx
@@ -0,0 +1,38 @@
+/**
+ * External modules
+ */
+import React from "react";
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  text-align: center;
+  font-size: 30px;
+  line-height: 36px;
+  font-weight: 500;
+`;
+
+export const H2 = styled.h2`
+  text-align: center;
+  font-size: 24px;
+  line-height: 32px;
+  font-weight: 500;
+  margin-top: 16px;
+  margin-bottom: 8px;
+`;
+
+export const H3 = styled.h3`
+  /* text-align: center; */
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 500;
+  margin-top: 16px;
+  margin-bottom: 8px;
+`;
+
+// export const H1 = ({ children }: React.PropsWithChildren) => {
+//   return <h1 className="text-center text-xl">{children}</h1>
+// };
+
+// export const H2 = ({ children }: React.PropsWithChildren) => {
+//   return <h2 className="text-lg text-center">{children}</h2>
+// };
