cfef1ab06107b5fff3bb433b573e9888a31e119d
Jaewook Ahn <dev.jaewook@gmail.com>
2023-03-09 15:27:24 +0900

    improve(page): Improve 404, 500 error page
    - add images for 404, 500 page
    - fix type error in 404, 500 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,27 @@
 /**
-* External modules
-*/
+ * External modules
+ */
 import React from "react";
+import { RiFileWarningLine } from "react-icons/ri";
 
 /**
-* Internal modules
-*/
+ * Internal modules
+ */
 import { Button } from "../components/Button";
 import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
 const NotFoundPage = () => {
-    const siteMetadata = useSiteMetadata();
+  const siteMetadata = useSiteMetadata();
 
-    return (
-        <div className="mt-32 mb-10 flex flex-col items-center justify-center">
-            <h1 className="text-2xl">페이지를 찾을 수 없어요.</h1>
-            <Button className="mt-5" onClick={() => window.location.href = siteMetadata?.siteUrl}>메인으로 가기</Button>
-        </div>
-    );
+  return (
+    <div className="mt-32 mb-10 flex flex-col items-center justify-center">
+      <RiFileWarningLine size={128} />
+      <h1 className="mt-8 text-2xl">페이지를 찾을 수 없어요.</h1>
+      <Button className="mt-5" onClick={() => (window.location.href = siteMetadata!.siteUrl!)}>
+        메인으로 가기
+      </Button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,22 +2,26 @@
  * External modules
  */
 import React from "react";
+import { TbPray } from "react-icons/tb";
 
 /**
-* Internal modules
-*/
+ * Internal modules
+ */
 import { Button } from "../components/Button";
 import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
 const ErrorPage = () => {
-    const queryResult = useSiteMetadata()
+  const siteMetadata = useSiteMetadata();
 
-    return (
-        <div className="mt-32 mb-10 flex flex-col items-center justify-center">
-            <h1 className="text-2xl">이런, 문제가 발생했어요!</h1>
-            <Button className="mt-5" onClick={() => window.location.href = queryResult.site.siteMetadata.siteUrl}>메인으로 가기</Button>
-        </div>
-    );
+  return (
+    <div className="mt-32 mb-10 flex flex-col items-center justify-center">
+      <TbPray size={128} />
+      <h1 className="mt-8 text-2xl">이런, 문제가 발생했어요!</h1>
+      <Button className="mt-5" onClick={() => (window.location.href = siteMetadata!.siteUrl!)}>
+        메인으로 가기
+      </Button>
+    </div>
+  );
 };
 
 export default ErrorPage;
