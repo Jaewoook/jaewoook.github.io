8cb0282d2c9c38dec2386eafccbda36aa803de7c
Jaewook Ahn <dev.jaewook@gmail.com>
2023-03-22 20:45:41 +0900

    fix(list): Update list marker position to inside

--- src/components/typography/List.ts
+++ src/components/typography/List.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const OL = styled.ol`
   > li {
     list-style-type: decimal;
+    list-style-position: inside;
     margin-bottom: 0.5em;
     word-wrap: break-word;
   }
@@ -11,6 +12,7 @@ export const OL = styled.ol`
 export const UL = styled.ul`
   > li {
     list-style-type: square;
+    list-style-position: inside;
     margin-bottom: 0.5em;
     word-wrap: break-word;
   }
