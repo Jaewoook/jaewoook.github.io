dffb99591adea721c9a6a2261190f5ac85b45849
Jaewook Ahn <dev.jaewook@gmail.com>
2024-01-07 21:22:25 +0900

    feat(theme): set dark mode colors for typography

--- src/components/typography/Paragraph.tsx
+++ src/components/typography/Paragraph.tsx
@@ -5,19 +5,19 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 export const P = styled.p`
+  ${tw`text-zinc-800 dark:text-zinc-200`}
   font-size: 18px;
   line-height: 32px;
   margin-bottom: 18px;
-  ${tw`text-zinc-800`}
-  `;
+`;
 
 export const BlockQuote = styled.blockquote`
+  ${tw`text-zinc-800 dark:text-zinc-200 dark:bg-neutral-800`}
   font-size: 18px;
   line-height: 32px;
   margin-bottom: 18px;
   background: #efefef;
   padding: 8px 16px;
-  ${tw`text-zinc-800`}
   > p {
     margin-bottom: 0;
   }
