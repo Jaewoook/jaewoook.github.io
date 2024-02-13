a48ff773ddff942776a4e4ae9b591cdf8252d119
Jaewook Ahn <dev.jaewook@gmail.com>
2024-02-13 18:55:31 +0900

    chore(deps): upgrade project dependencies
    - migrate styled-components to v6

--- src/components/blog/Layout.tsx
+++ src/components/blog/Layout.tsx
@@ -1,8 +1,9 @@
 /**
  * External modules
  */
-import React from "react";
-import styled from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
+import React, { useCallback } from "react";
+import styled, { StyleSheetManager } from "styled-components";
 
 /**
  * Internal modules
@@ -10,6 +11,8 @@ import styled from "styled-components";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Progress } from "../common/Progress";
+
+import type { ShouldForwardProp } from "styled-components";
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -24,13 +27,23 @@ const Wrapper = styled.div`
 const Layout = (props: React.PropsWithChildren) => {
   const { children } = props;
 
+  const shouldForwardProp = useCallback<ShouldForwardProp<"web">>((propName, target) => {
+    if (typeof target === "string") {
+      return isPropValid(propName);
+    }
+
+    return true;
+  }, []);
+
   return (
-    <Wrapper className="flex flex-col bg-neutral-50 dark:bg-neutral-900 transition-colors">
-      <Header />
-      <main className="flex-1 container mx-auto">{children}</main>
-      <Footer />
-      <Progress />
-    </Wrapper>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <Wrapper className="flex flex-col bg-neutral-50 dark:bg-neutral-900 transition-colors">
+        <Header />
+        <main className="flex-1 container mx-auto">{children}</main>
+        <Footer />
+        <Progress />
+      </Wrapper>
+    </StyleSheetManager>
   );
 };
 
