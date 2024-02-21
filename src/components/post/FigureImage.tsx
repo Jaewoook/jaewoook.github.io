2a4a96df74cc1866a6b427cc027b2e2c59bd4961
Jaewook Ahn <dev.jaewook@gmail.com>
2024-02-21 20:43:48 +0900

    style(image): adjust figure image caption text style

--- src/components/post/FigureImage.tsx
+++ src/components/post/FigureImage.tsx
@@ -2,6 +2,7 @@
  * External modules
  */
 import styled from "styled-components";
+import tw from "twin.macro";
 
 /**
  * Internal modules
@@ -9,9 +10,11 @@ import styled from "styled-components";
 import { P } from "../typography/Paragraph";
 
 const Figcaption = styled(P).attrs({ as: "figcaption" })`
+  ${tw`text-zinc-600 dark:text-zinc-400`}
   font-style: italic;
   text-align: center;
   font-size: 14px;
+  font-weight: 300;
   line-height: 20px;
 `;
 
