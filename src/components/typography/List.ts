c51d87deceddadaad96117bc01494acbc046a4e9
Jaewook Ahn <dev.jaewook@gmail.com>
2024-05-29 15:05:48 +0900

    style(typography): adjust spacing of list and paragraph tag

--- src/components/typography/List.ts
+++ src/components/typography/List.ts
@@ -13,25 +13,35 @@ import type { SpaceProps } from "styled-system";
 export const OL = styled.ol<SpaceProps>`
   ${space}
   ${tw`text-zinc-700 dark:text-zinc-300`}
-  padding-top: 0.5em;
+  padding-top: 0.25em;
+  padding-bottom: 0.25em;
   padding-inline-start: 1.125em;
   & > li {
     list-style-type: decimal;
-    margin-bottom: 0.25em;
     word-wrap: break-word;
     word-break: keep-all;
+    line-height: 32px;
+  }
+  & ol {
+    padding-top: 0;
+    padding-bottom: 0;
   }
 `;
 
 export const UL = styled.ul<SpaceProps>`
   ${space}
   ${tw`text-zinc-700 dark:text-zinc-300`}
-  padding-top: 0.5em;
+  padding-top: 0.25em;
+  padding-bottom: 0.25em;
   padding-inline-start: 1.125em;
   & > li {
     list-style-type: disc;
-    margin-bottom: 0.25em;
     word-wrap: break-word;
     word-break: keep-all;
+    line-height: 32px;
+  }
+  & ul {
+    padding-top: 0;
+    padding-bottom: 0;
   }
 `;
