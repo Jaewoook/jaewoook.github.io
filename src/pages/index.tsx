7ce1c3b15b47e7c772d53cf28fb28b580aef8f91
Jaewook Ahn <dev.jaewook@gmail.com>
2025-01-28 21:02:52 +0900

    refactor(ui): redesign post card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
-/**
- * External modules
- */
 import { useMemo } from "react";
 import { graphql } from "gatsby";
 import { useRecoilValue } from "recoil";
 
-/**
- * Internal modules
- */
 import { Category, PostCard, SEO } from "@/components/blog";
 import { selectedCategoryState } from "@/states/category";
 
-/**
- * Type modules
- */
 import type { HeadFC, PageProps } from "gatsby";
 
 export const query = graphql`
@@ -33,7 +24,7 @@ export const query = graphql`
           secret
           hero {
             childImageSharp {
-              gatsbyImageData(width: 400 placeholder: BLURRED)
+              gatsbyImageData(width: 350, placeholder: BLURRED)
             }
           }
         }
@@ -42,35 +33,45 @@ export const query = graphql`
   }
 `;
 
-
 const Index = (props: PageProps<Queries.AllPostInfoQuery>) => {
-  const { data: { allMdx: allPostInfo } } = props;
+  const {
+    data: { allMdx: allPostInfo },
+  } = props;
   const selectedCategory = useRecoilValue(selectedCategoryState);
   const categories = useMemo(() => {
     const c = allPostInfo.nodes.map((post) => post.frontmatter?.category ?? "").filter((c) => !!c);
     const catSet = new Set<string>(c);
     return Array.from(catSet);
   }, [allPostInfo]);
+  const posts = useMemo(() => {
+    return allPostInfo.nodes.filter(
+      (post) => selectedCategory.index === 0 || selectedCategory.name === post.frontmatter?.category
+    );
+  }, [allPostInfo, selectedCategory]);
 
   return (
     <div className="container mx-auto pt-6 max-sm:pt-4">
-      <Category mb={["16px", "24px"]} categories={categories} selectedIndex={selectedCategory.index} />
-      <section className="flex flex-1 flex-wrap gap-4">
-        {allPostInfo.nodes.map((node) => {
-          return (
-            <PostCard
-              className={selectedCategory.index === 0 || selectedCategory.name === node.frontmatter?.category ? "flex" : "hidden"}
-              key={node.id}
-              title={node.frontmatter?.title ?? ""}
-              excerpt={node.excerpt ?? ""}
-              category={node.frontmatter?.category ?? ""}
-              date={node.frontmatter?.date ?? ""}
-              image={node.frontmatter?.hero?.childImageSharp?.gatsbyImageData}
-              slug={node.frontmatter?.slug ?? ""}
-              secret={node.frontmatter?.secret ?? false}
-            />
-          );
-        })}
+      <Category
+        mb={["16px", "24px"]}
+        categories={categories}
+        selectedIndex={selectedCategory.index}
+      />
+      <section className="mt-12">
+        <div className="grid grid-cols-5 gap-x-6 gap-y-9 max-2xl:grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1">
+          {posts.map((post) => {
+            return (
+              <PostCard
+                key={post.id}
+                title={post.frontmatter?.title ?? ""}
+                excerpt={post.excerpt ?? ""}
+                category={post.frontmatter?.category ?? ""}
+                image={post.frontmatter?.hero?.childImageSharp?.gatsbyImageData}
+                slug={post.frontmatter?.slug ?? ""}
+                secret={post.frontmatter?.secret ?? false}
+              />
+            );
+          })}
+        </div>
       </section>
     </div>
   );
